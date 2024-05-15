cc5343301b2b42a5768a9c09f1893e88ac403f23
Arvind Venkat Sai Vummadisingu <arvindsaiv@Arvinds-MacBook-Pro.local>
2024-05-15 16:24:02 -0400

    Added the base and site in astro config for github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://the-boxing-reformer.com',
 	integrations: [mdx(), sitemap()],
+	base: 'the-boxing-reformer'
 });
